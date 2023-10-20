2334f0e136474a82fe67f5a1497d0fb955aa0a59
Hakan Osman <haki2001@abv.bg>
2023-10-20 10:41:45 +0300

    comiit

---
+++ Demo-Lab/Program.cs
@@ -0,0 +1,48 @@
+﻿
+using Microsoft.Data.SqlClient;
+using System.Text.RegularExpressions;
+
+namespace FirstExerciese
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString =
+                @"Server =(LocalDB)\MSSQLLocalDB; 
+             Database = SoftUni; Trusted_Connection = True;";
+             GetAllEmployeesLastName(connectionString).GetAwaiter().GetResult();
+
+
+        }
+        private static async Task GetAllEmployeesLastName(string connectionString)
+        {
+
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                await sqlConnection.OpenAsync();
+                string query = @"SELECT FirstName" + ",LastName" + ",Salary FROM Employees " +
+                    "WHERE Salary > @salaryParam";
+                using (SqlCommand sqlCommand = new SqlCommand(query, sqlConnection))
+                {
+                    sqlCommand.Parameters.AddWithValue("@salaryParam", 50000);
+
+                    SqlDataReader sqlDataReader =  await sqlCommand.ExecuteReaderAsync();
+                    while ( await sqlDataReader.ReadAsync())
+                    {
+
+                        string firstName = sqlDataReader[0].ToString();
+                        string lastName = sqlDataReader[1].ToString();
+                        decimal salary = decimal.Parse(sqlDataReader[2].ToString());
+                        Console.WriteLine($"{firstName} - {lastName} with salary {salary:f2}");
+                    }
+
+                }
+
+
+            }
+
+        }
+
+    }
+}
