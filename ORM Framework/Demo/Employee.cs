69e24ab71e10dbb01610c95da5d59df55d574e21
Hakan Osman <haki2001@abv.bg>
2023-10-27 10:19:40 +0300

    commit

---
+++ ORM Framework/Demo/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [ForeignKey (nameof(Department))]
+        public int DepartmentId { get; set; }
+        public Department Department { get; set; }
+    }
+}
---
+++ ORM Framework/Demo/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Demo
+{
+    internal class Program
+    {
+        //private static HashSet<string> permutaions;
+        static void Main(string[] args)
+        {
+
+            var context = new ApplicationDbContext();
+            var result=from  employee in context.Employees 
+                       where employee.FirstName=="Pesho"
+                       select employee;
+            var result1=
+                context.Employees
+                .Where(e=>e.FirstName=="Pesho").ToList();
+        }
+
+        
+    }
+}
