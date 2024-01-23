a55dcffe6b24ec3a5583d1c4fa4a341c0f15f07e
Hakan Osman <haki2001@abv.bg>
2024-01-23 15:25:54 +0200

    commit

---
+++ C# Auto Mapping Objects/07.Auto-Mapping-Objects-Exercises-FastFood-6.0 (1)/FastFood.Core/MappingConfiguration/FastFoodProfile.cs
@@ -0,0 +1,29 @@
+﻿namespace FastFood.Core.MappingConfiguration
+{
+    using AutoMapper;
+    using FastFood.Core.ViewModels.Categories;
+    using FastFood.Models;
+    using ViewModels.Positions;
+
+    public class FastFoodProfile : Profile
+    {
+        public FastFoodProfile()
+        {
+            //Positions
+            CreateMap<CreatePositionInputModel, Position>()
+                .ForMember(x => x.Name, y => y.MapFrom(s => s.PositionName));
+
+            CreateMap<Position, PositionsAllViewModel>()
+                .ForMember(x => x.Name, y => y.MapFrom(s => s.Name));
+            //Categories
+            CreateMap<CreateCategoryInputModel,Category>()
+                .ForMember(x=>x.Name,y=>y.MapFrom(s=>s.CategoryName));
+            CreateMap<Category, CategoryAllViewModel>()
+                .ForMember(x => x.Name, y => y.MapFrom(s => s.Name));
+
+             
+
+
+        }
+    }
+}
